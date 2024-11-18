682bac567a0e04b564a5620dbc10f105e2dd03f1
Qunoot-Abdulla <qunoot.al.sada@gmail.com>
2024-11-18 15:28:12 +0300

    Third

--- record.php
+++ record.php
@@ -1,4 +1,5 @@
-<!--a PHP application that retrieves and displays UOB student nationality data from the Bahrain Open Data Portal using their public API.-->
+<!--a PHP application that retrieves and displays UOB student nationality data from the Bahrain Open Data Portal using their public API.
+The PHP file is done by Qunoot Sayed Mahdi Khalil Ali Abdulla (202209102)-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,12 +11,10 @@
 <body>
     <h1>UOB Student Nationality Data</h1>
     <?php
-    // PHP code to retrieve and process data
     $URL = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
     $response = file_get_contents($URL);
     $result = json_decode($response, true);
     if ($result) {
-        // Start the table
         echo '<table>';
         echo '<thead>';
         echo '<tr><th>Year</th><th>Semester</th><th>Nationality</th><th>Number of Students</th></tr>';
@@ -32,9 +31,9 @@
             echo '</tr>';
         }
         echo '</tbody>';
-        echo '</table>'; // End the table
+        echo '</table>'; 
     } else {
-        echo '<p>No data available.</p>'; // Handle case where no data is returned
+        echo '<p>No data available.</p>'; 
     }
     ?>
 </body>
